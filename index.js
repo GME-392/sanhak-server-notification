36976824bec47bf251eeaca44a755bcd3e30ee7d
SongGol <dldmsgus2@gmail.com>
2021-05-04 15:27:36 +0900

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //const axios = require('axios');
-import axios from "axios";
+//import axios from "axios";
 
 let USER_ENDPOINT = "https://tdwcyrlp8g.execute-api.ap-northeast-2.amazonaws.com/noticeDB";
 let arr = [];

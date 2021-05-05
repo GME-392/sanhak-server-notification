cb3c8a9f914a747e9a0e07de33f8fc039c5c077b
SongGol <dldmsgus2@gmail.com>
2021-05-05 23:52:16 +0900

    .

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ function pushInfo() {
 function tableDelete(obj, id) {
     let tr = $(obj).parent().parent();
     tr.remove();
-    let content = (arr.filter(data => data["id"] == parseInt(id)))["content"];
+    let content = arr.findIndex(x => x["content"] == parseInt(id));
     deleteContestElement(content);
-    arr = arr.filter(data => data["id"] != parseInt(id));
+    arr = arr.findIndex(x => x["content"] != parseInt(id))
 }
 
 //db에서 제거.

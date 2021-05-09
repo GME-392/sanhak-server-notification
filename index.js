8b414fda1a63b76d8a569150ddbf6b262bee9da0
SongGol <dldmsgus2@gmail.com>
2021-05-09 16:55:45 +0900

    채용정보관련 추가

--- index.js
+++ index.js
@@ -1,93 +1,32 @@
-let USER_ENDPOINT = "https://tdwcyrlp8g.execute-api.ap-northeast-2.amazonaws.com/noticeDB";
-let arr = [];
+const USER_ENDPOINT_CODE = "https://tdwcyrlp8g.execute-api.ap-northeast-2.amazonaws.com/noticeDB";
+const USER_ENDPOINT_JOB = "https://qojk0ukeu2.execute-api.ap-northeast-2.amazonaws.com/infoDB";
+let contestInfo = [];
 let jobInfo = [];
-let g_id = -1;
-let MAXID = 10000;
 
-function pushInfo() {
-    let date = document.getElementById("contest-date").value;
-    let content = document.getElementById("contest-content").value;
-    let link = document.getElementById("contest-link").value;
-    let id = content+date;
-    let obj = {
-        "date": date,
-        "content": content,
-        "link": link,
-        "id": id,
-    };
-    
-    let elems = '';
-    elems += "<tr><td>" + date
-        + "</td><td>" + content
-        + "</td><td>" + link
-        + "</td><td><button onclick='tableDelete(this,"+ `"${id}"` +")'>X</button></td></tr>";
-    $("#contest-table").append(elems);
-    arr.push(obj);
-
-    $("#contest-date").val("");
-    $("#contest-content").val("");
-    $("#contest-link").val("");
-
-    //db로 전송하는 함수
-    postToDB(content, date, link, id);
-}
-
-//테이블에서 제거
-function tableDelete(obj, id) {
-    let tr = $(obj).parent().parent();
-    tr.remove();
-    let contentIndex = arr.findIndex(x => x["id"] == id);
-    deleteContestElement(id);
-    arr.splice(contentIndex, 1);
-}
-
-//db에서 제거.
-//차후에는 db에 id필드값을 추가하든 방법을 찾아야 할 듯
-async function deleteContestElement(id) {
-  console.log(id);
-  try {
-    await axios
-      .delete(USER_ENDPOINT, {
-        data: {
-          "id": `${id}`,
-        },
-      })
-      .then(response => console.log(response));
-  } catch(err) {
-    console.log(err);
-  }
-}
-
-//aws endpoint
-//contest info
-//https://tdwcyrlp8g.execute-api.ap-northeast-2.amazonaws.com/noticeDB
-async function postToDB(infoName, date, link, id) {
-  try {
-    await axios
-      .post(USER_ENDPOINT, {
-        "infoName": infoName,
-        "date": date,
-        "link": link,
-        "id": id,
-      })
-      .then(response => console.log(response));
-  } catch(err) {
-    console.log(err);
-  }
-}
-
+//db에서 모든 값 가져오기
 async function getAllNotice() {
   try {
     await axios
-      .get(`${USER_ENDPOINT}?func=getAllNotice`, {})
+      .get(`${USER_ENDPOINT_CODE}?func=getAllNotice`, {})
       .then(res => {
         setContestTable(res.data);
       });
   } catch(err) {
     console.log(err);
   }
+
+  try {
+    await axios
+      .get(`${USER_ENDPOINT_JOB}?func=getAllNotice`, {})
+      .then(res => {
+        setJobTable(res.data);
+      });
+  } catch(err) {
+    console.log(err);
+  }
 }
 
+//contest 테이블 설정
 function setContestTable(res) {
   for(let element of res) {
     let date = element["date"];
@@ -105,8 +44,155 @@ function setContestTable(res) {
     elems += "<tr><td>" + date
         + "</td><td>" + content
         + "</td><td>" + link
-        + "</td><td><button onclick='tableDelete(this,"+ `"${id}"` +")'>X</button></td></tr>";
+        + "</td><td><button onclick='deleteContestTableElement(this,"+ `"${id}"` +")'>X</button></td></tr>";
     $("#contest-table").append(elems);
-    arr.push(obj);
+    contestInfo.push(obj);
+  }
+}
+
+//job 테이블 설정
+function setJobTable(res) {
+  for(let element of res) {
+    let date = element["date"];
+    let content = element["infoName"];
+    let link = element["link"];
+    let id = element["id"];
+    let obj = {
+      "date": date,
+      "content": content,
+      "link": link,
+      "id": id,
+    };
+    let elems = '';
+    elems += "<tr><td>" + date
+        + "</td><td>" + content
+        + "</td><td>" + link
+        + "</td><td><button onclick='deleteJobTableElement(this,"+ `"${id}"` +")'>X</button></td></tr>";
+    $("#job-table").append(elems);
+    jobInfo.push(obj);
+  }
+}
+
+//contest table에 요소 추가, db에 post요청
+function addContestElement() {
+  let date = document.getElementById("contest-date").value;
+  let content = document.getElementById("contest-content").value;
+  let link = document.getElementById("contest-link").value;
+  let id = content+date;
+  let obj = {
+      "date": date,
+      "content": content,
+      "link": link,
+      "id": id,
+  };
+  
+  let elems = '';
+  elems += "<tr><td>" + date
+      + "</td><td>" + content
+      + "</td><td>" + link
+      + "</td><td><button onclick='deleteContestTableElement(this,"+ `"${id}"` +")'>X</button></td></tr>";
+  $("#contest-table").append(elems);
+  contestInfo.push(obj);
+
+  $("#contest-date").val("");
+  $("#contest-content").val("");
+  $("#contest-link").val("");
+
+  //db로 전송하는 함수 
+  postToDB(USER_ENDPOINT_CODE, content, date, link, id);
+}
+
+//job table에 요소 추가, db에 post요청
+function addJobElement() {
+  let date = document.getElementById("job-date").value;
+  let content = document.getElementById("job-content").value;
+  let link = document.getElementById("job-link").value;
+  let id = content+date;
+  let obj = {
+      "date": date,
+      "content": content,
+      "link": link,
+      "id": id,
+  };
+  
+  let elems = '';
+  elems += "<tr><td>" + date
+      + "</td><td>" + content
+      + "</td><td>" + link
+      + "</td><td><button onclick='deleteJobTableElement(this,"+ `"${id}"` +")'>X</button></td></tr>";
+  $("#job-table").append(elems);
+  jobInfo.push(obj);
+
+  $("#job-date").val("");
+  $("#job-content").val("");
+  $("#job-link").val("");
+
+  //db로 전송하는 함수 
+  postToDB(USER_ENDPOINT_JOB, content, date, link, id);
+}
+
+//code 테이블에서 제거
+function deleteContestTableElement(obj, id) {
+  let tr = $(obj).parent().parent();
+  tr.remove();
+  let contentIndex = contestInfo.findIndex(x => x["id"] == id);
+  deleteContestElement(id);
+  contestInfo.splice(contentIndex, 1);
+}
+
+//job 테이블에서 제거
+function deleteJobTableElement(obj, id) {
+  let tr = $(obj).parent().parent();
+  tr.remove();
+  let contentIndex = jobInfo.findIndex(x => x["id"] == id);
+  deleteJobElement(id);
+  jobInfo.splice(contentIndex, 1);
+}
+
+//Contest 정보 db에서 제거
+async function deleteContestElement(id) {
+  console.log(id);
+  try {
+    await axios
+      .delete(USER_ENDPOINT_CODE, {
+        data: {
+          "id": `${id}`,
+        },
+      })
+      .then(response => console.log(response));
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+//job 정보 db에서 제거
+async function deleteJobElement(id) {
+  console.log(id);
+  try {
+    await axios
+      .delete(USER_ENDPOINT_JOB, {
+        data: {
+          "id": `${id}`,
+        },
+      })
+      .then(response => console.log(response));
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+//db의 endpoint에 정보 post
+async function postToDB(user_endpoint, infoName, date, link, id) {
+  try {
+    await axios
+      .post(user_endpoint, {
+        "infoName": infoName,
+        "date": date,
+        "link": link,
+        "id": id,
+      })
+      .then(response => console.log(response));
+  } catch(err) {
+    console.log(err);
   }
 }

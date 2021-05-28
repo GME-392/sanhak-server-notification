84200a10ed9e61f3cb7544a671b56464fa08a190
SongGol <dldmsgus2@gmail.com>
2021-05-28 19:59:03 +0900

    크롤러관련 정상작동

--- crawler/jobkorea-crawler.js
+++ crawler/jobkorea-crawler.js
@@ -1,41 +1,31 @@
-var axios = require('axios');
-var cheerio = require('cheerio');
+let axios = require('axios');
+let cheerio = require('cheerio');
 
-//event, context, callback
-exports.handler = async () => {
+const url = "https://www.jobkorea.co.kr/recruit/joblist?menucode=duty&dutyCtgr=10016";
+
+exports.handler = async (event, context, callback) => {
     // 사용 변수들
-    //let User_ID = event.name;
-    let url = "https://www.jobkorea.co.kr/recruit/joblist?menucode=duty&dutyCtgr=10016";
-    let solve_num = []; // 지금까지 푼 문제들을 저장해 놓은 배열  
-    
+    let jobList = []; // 잡코리아 채용정보를 저장할 배열
     let today = new Date();   
 
     let year = today.getFullYear(); // 년도
     let year_str = year + "-";
-    let month = today.getMonth() + 1;  // 월
-    let date = today.getDate();  // 날짜
-    let day = today.getDay();  // 요일
 
-    await axios.get(url).then(all_data => {
+    await axios.get(url)
+    .then(all_data => {
         // 성공적으로 data(html)를 받아왔다면
-        console.log("Success!!");
-        //console.log(all_data);
-
+        console.log("Success");
         //console.log(all_data.data);
+
         // cheerio 모듈을 사용하여 data를 저장하고
         let $ = cheerio.load(all_data.data);
-        // 해당 string은 내가 원하는 정보인 "지금까지 solve한 문제들"이 저장되어 있는 경로임!
 
+        //jobkorea에서 필요한 채용정보 추출하기
         const pre_url = "https://www.jobkorea.co.kr";
         const $bodyList = $('body>div#wrap>div#container>div#content>div.rcr_cnt>div#dev-gi-list>div.tplJobListWrap>div.tplList>table>tbody>tr');
+        console.log("bodyList");
+        console.log($bodyList);
         $bodyList.each((index, item) => {
-            //console.log($(item).find("td.tplCo>a").text()); 회사명
-            //console.log($(item).find("td.tplTit>div.titBx>strong>a").text()); 공고 제목
-            //console.log(pre_url+$(item).find("td.tplTit>div.titBx>strong>a")[0].attribs.href); 공고 링크
-            //console.log($(item).find("td.odd>span.date>span.tahoma").text().substr(1).split("/")); 공고 날짜
-            //console.log(year+"-"+$(item).find("td.odd>span.date>span.tahoma").text().substr(1).replace("/","-"));
-            //console.log($(item).find("td.odd>span.date").text()); 뒤 요일까지 붙어서 나옴.
-
             let d = year+"-"+$(item).find("td.odd>span.date>span.tahoma").text().substr(1).replace("/","-");
             d = d == year_str ? "상시채용" : d;
             let c = $(item).find("td.tplCo>a").text()+ " " +$(item).find("td.tplTit>div.titBx>strong>a").text();
@@ -45,69 +35,26 @@ exports.handler = async () => {
                 content: c,
                 link: pre_url+$(item).find("td.tplTit>div.titBx>strong>a")[0].attribs.href,
                 id: d+c,
-            }
-            solve_num.push(obj);
-        })
-    })
-    .catch(err => {console.log(err);});
-    console.log(solve_num);
-    return {
-        statusCode : 200,
-        body : solve_num
-    }
-};
-exports.handler();
-
-/*
-//event, context, callback
-exports.handler = async () => {
-    
-    // 사용 변수들
-    //let User_ID = event.name;
-    let User_ID = "ghtn5706";
-    let url = `https://www.acmicpc.net/user/${User_ID}`;
-    let solve_num = []; // 지금까지 푼 문제들을 저장해 놓은 배열  
-    console.log(url);
-    await axios.get(url).then(all_data => {
-        // 성공적으로 data(html)를 받아왔다면
-        console.log("Success!!");
-
-        //console.log(all_data.data);
-         // cheerio 모듈을 사용하여 data를 저장하고
-        let $ = cheerio.load(all_data.data);
-        // 해당 string은 내가 원하는 정보인 "지금까지 solve한 문제들"이 저장되어 있는 경로임!
-        $('body>div.wrapper>div.container>div.row>div.col-md-12>div.row>div.col-md-9>div.panel:nth-child(1)>div.panel-body>a').each((index,item) => {
-            solve_num.push(item.attribs.href.substr(9)); // 지금까지 푼 문제  번호들 저장
+            };
+            jobList.push(obj);
         });
-            
-        })
-    .catch(err => {console.log(err);});
-    console.log(solve_num);
+    })
+    .catch(err => {
+        console.log(err);
+        callback(null, {
+            statusCode : 400,
+            body : JSON.stringify("error crawler get"),
+        });
+    });
+    console.log(today);
+    console.log(jobList);
+    
     return {
         statusCode : 200,
-        body : solve_num
-    } 
-
+        body : jobList
+    };
+    // callback(null, {
+    //     statusCode : 200,
+    //     body : JSON.stringify(jobList)
+    // });
 };
-exports.handler();
-*/
-/*
-request("https://www.reddit.com", function(error, response, body) {
-  if(error) {
-    console.log("Error: " + error);
-  }
-  console.log("Status code: " + response.statusCode);
-
-  var $ = cheerio.load(body);
-
-  $('div#siteTable > div.link').each(function( index ) {
-    var title = $(this).find('p.title > a.title').text().trim();
-    var score = $(this).find('div.score.unvoted').text().trim();
-    var user = $(this).find('a.author').text().trim();
-    console.log("Title: " + title);
-    console.log("Score: " + score);
-    console.log("User: " + user);
-    fs.appendFileSync('reddit.txt', title + '\n' + score + '\n' + user + '\n');
-  });
-
-});*/
--- rest-api/jobDB.js
+++ rest-api/jobDB.js
@@ -1,6 +1,6 @@
 let axios = require("axios");
 
-const USER_ENDPOINT_JOB = "https://qojk0ukeu2.execute-api.ap-northeast-2.amazonaws.com/infoDB/";
+const USER_ENDPOINT_JOB = "https://qojk0ukeu2.execute-api.ap-northeast-2.amazonaws.com/infoDB";
 const JOB_KOREA_ENDPOINT = "https://tt39zwn1ba.execute-api.ap-northeast-2.amazonaws.com/stage1/crawler/";
 let jobList = [];
 
@@ -11,11 +11,11 @@ async function getJobPostDB() {
         .get(JOB_KOREA_ENDPOINT, {})
         .then(res => {
             jobList = res.data.body;
-            console.log(res);
-            console.log(1111111111);
+            //console.log(`id: ${jobList[0]["id"]}, date: ${jobList[0]["date"]}, content: ${jobList[0]["content"]}`);
             postToDB(jobList[0]);
-            for(let index in jobList) {
-                //postToDB(jobList[index]);
+            for(let i in jobList) {
+                //console.log(jobList[i]);
+                postToDB(jobList[i]);
             }
         });
     } catch(err) {
@@ -26,26 +26,26 @@ async function getJobPostDB() {
 //db의 endpoint에 정보 post
 async function postToDB(obj) {
     try {
-      await axios
-        .post(USER_ENDPOINT_JOB, {
-        "infoName": obj["content"],
-        "date": obj["date"],
-        "link": obj["link"],
-        "id": obj["id"],
-      })
-      .then(response => console.log(response));
+        await axios
+            .post(USER_ENDPOINT_JOB, {
+            "infoName": obj["content"],
+            "date": obj["date"],
+            "link": obj["link"],
+            "id": obj["id"],
+        })
+        .then(response => console.log(response));
     } catch(err) {
         console.log(err);
     }
 }
 
 
-exports.handler = async (event) => {
+exports.handler = (event) => {
     // TODO implement
     getJobPostDB();
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('push to jobDB'),
     };
     return response;
 };
